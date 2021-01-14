39483465cbfa03eb2b0d0ec0f3f39979d3b1e785
Devin Flora <devinlflora@gmail.com>
2021-01-14 16:16:58 -0600

    first commit

---
+++ MovieWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieWatch
+//
+//  Created by Devin Flora on 1/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
